7abfa6b676dc63bd054c56d37ba0fef84b5a7fe1
david <miner2.blablmgmail.co>
2022-04-26 20:32:21 +0200

    Fixed script

--- inject_path.sh
+++ inject_path.sh
@@ -3,4 +3,10 @@ cd "$(dirname "$0")"
 path=$(/bin/pwd)
 temp="export PATH="$"PATH:${path}"
 echo "$temp" >> ~/.bashrc
-echo "alias tagdb='tagdb'" >> ~/.bashrc
+echo "tagdb_fn(){
+       if [ "$"# -eq 0 ]; then
+           echo "No arguments provided!"
+       fi
+       (cd $path
+       ./tagdb ""$"*")}" >> ~/.bashrc
+echo "alias tagdb='tagdb_fn'" >> ~/.bashrc
